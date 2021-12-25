b72ac97658704ff7fc965a4c0cb47ef224f4e5ec
Abbos <adamsmidov02@gmail.com>
2021-12-25 11:51:05 +0500

    added function for taking input from microphone

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ import playsound
 import pyjokes
 import wikipedia
 import pyaudio
+
+# get mic audio
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception " + str(e))
+    return said
+
+# def 
